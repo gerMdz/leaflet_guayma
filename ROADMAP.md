7447bf8714aa9909d67cab10cc6d17624310f99d
Gerardo Montivero <gmontivero@jus.mendoza.gov.ar>
2023-05-31 12:22:31 -0300

    :zap: Objetos

--- ROADMAP.md
+++ ROADMAP.md
@@ -11,4 +11,4 @@
 * console.error
 
 ##### Next
-(Cap 7)[https://www.udemy.com/course/leaflet-crea-mapas-interactivos-para-la-web/learn/lecture/36605938#overview]
+(Cap 9)[https://www.udemy.com/course/leaflet-crea-mapas-interactivos-para-la-web/learn/lecture/36606148#overview]
--- js/main.js
+++ js/main.js
@@ -5,3 +5,27 @@
   // boolean
   // undefined
 
+// Arreglos, inician en 0
+
+const arreglo = [1, 1.2 ,'Uno', true, undefined];
+
+// Métodos
+
+arreglo.forEach( elemento => {
+  console.log(elemento)
+})
+
+//Objetos literales
+
+const miObj = {
+  name: 'Juan',
+  lastName: 'Bensa',
+  age: '30',
+  salario: undefined,
+  tutorial: [
+    'leaflet', 'Laravel', 'Sf6'
+  ]
+}
+
+console.warn(miObj)
+console.table(miObj)
